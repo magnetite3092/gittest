9b0de401a5a2340b4cc1b911549d4894e7741fd7
magnetite3092 <lunahana7110magnetite@gmail.com>
2026-09-02 01:16:17 +0900

    main側で足し算表示を変更

--- math.py
+++ math.py
@@ -2,7 +2,7 @@ print("簡単な計算プログラム")
 num1 = int(input("1つ目の数字: "))
 num2 = int(input("2つ目の数字: "))
 
-print("足し算:", num1 + num2)
+print("2つの数字の合計:", num1 + num2)
 print("引き算:", num1 - num2)
 
 print("掛け算:", num1 * num2)
